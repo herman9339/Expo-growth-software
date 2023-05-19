5ad4c65c4e958ea2d090f66d377f6cd3c13b912d
herman9339 <78606095+herman9339@users.noreply.github.com>
2023-05-19 16:36:38 -0700

    Update main.js

--- projects/Myavatar-photo grouping 3.0/main.js
+++ projects/Myavatar-photo grouping 3.0/main.js
@@ -164,6 +164,23 @@ document.getElementById('downloadButton').addEventListener('click', async functi
 });
 
   
+var modal = document.getElementById("popUpWindow");
+var btn = document.getElementById("InstructionPopUpButton");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
